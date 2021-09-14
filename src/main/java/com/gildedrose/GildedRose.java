22a40ae25c9bb78567a094c5187132c00aac2f35
jlob275 <68893201+jlob275@users.noreply.github.com>
2021-09-15 03:57:57 +1200

    Refactor GildedRose class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,61 +9,59 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            int dayStartQuality = items[i].quality;
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            int qualityModifier = -1;
+            switch (items[i].name) {
+                case "Sulfuras, Hand of Ragnaros":
+                    // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+                    qualityModifier = 0;
+                    break;
+                
+                case "Aged Brie":
+                    // "Aged Brie" actually increases in Quality the older it gets
+                    qualityModifier = 1;
+                    break;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                case "Conjured":
+                    // "Conjured" items degrade in Quality twice as fast as normal items
+                    qualityModifier *= 2;
+                    break;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    // "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	                // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	                // Quality drops to 0 after the concert
+                    int sellIn = items[i].sellIn;
+                    if (sellIn <= 0) {
+                        items[i].quality = 0;
+                        qualityModifier = 0;
+                    } else if (sellIn <= 5) {
+                        qualityModifier = 3;
+                    } else if (sellIn <= 10) {
+                        qualityModifier = 2;
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        qualityModifier = 1;
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                    break;
             }
 
-            if (items[i].name.equals("Conjured")) {
-                items[i].quality = items[i].quality*2 - dayStartQuality;
-                if (items[i].quality < 0) {
+            // Once the sell by date has passed, Quality degrades twice as fast
+            if (items[i].sellIn <= 0) {
+                qualityModifier *= 2;
+            }
+
+            // enact quality change, but never set quality to more than 50 or less than 0
+            if (qualityModifier != 0) {
+                items[i].quality += qualityModifier;
+                if (items[i].quality > 50) {
+                    items[i].quality = 50;
+                } else if (items[i].quality < 0) {
                     items[i].quality = 0;
                 }
+            }
+
+            // Sulfuras never has to be sold
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                items[i].sellIn = items[i].sellIn - 1;
             }
         }
     }
